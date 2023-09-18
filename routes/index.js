1ecc003a9b46a12bef453bc6b6d6bbd4caff1a81
cloudyhead1 <ercx@fresnou.org>
2023-09-18 09:28:21 -0700

    commit index.js style.css

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,18 @@ router.get('/', function(req, res) {
 });
 module.exports = router;
 router.post('/card', function(req,res){
-  console.log(req.body.dateOfNBirth)
+  console.log(req.body.dateOfBirth)
   res.render('card', {
-    firstName:
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    type: req.body.type,
+    dataOfBirth: new Data(req.body.dataOfBirth),
+    addressLine1: neq.body.addressLine1,
+    addressLine2: neq.body.addressLine2,
+    city:req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    accountNumber: createAccountNumber(),
+    currentDate: new Data()
   });
 })
